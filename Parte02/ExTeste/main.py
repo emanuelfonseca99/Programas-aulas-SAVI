530358d8d260db518e10fd694fab5f81a5aa1ccf
Miguel Oliveira <m.riem.oliveira@gmail.com>
2022-09-27 09:59:05 +0100

    Test exercise

---
+++ Parte02/ExTeste/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import numpy as np  # shortcut or alias
+import cv2
+
+
+def main():
+
+    print('Creating a new image')
+    # image = np.ndarray((240,320,3), dtype=np.uint8)
+    image = np.random.randint(0, high=255, size=(240,320), dtype=np.uint8)
+    # when indexing matrices use a row,col (y,x) order
+
+    # Set image to gray
+    # image = image*0 + 128
+    # image += 128 # is the same as "image = image + 128"
+
+    cv2.imshow('window', image)
+    cv2.waitKey(0)
+
+if __name__ == "__main__": # checks if code was called from terminal
+    main() # call main function
