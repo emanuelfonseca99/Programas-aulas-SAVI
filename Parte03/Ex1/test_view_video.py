0e9a43cddd771cbfd51d51ef8c8c613329ed51a7
Miguel Oliveira <m.riem.oliveira@gmail.com>
2022-10-04 10:23:25 +0100

    Fix problems in Task1 - Program to view video frame by frame #1

---
+++ Parte03/Ex1/test_view_video.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import cv2
+import numpy as np
+
+
+def main():
+
+    cap = cv2.VideoCapture("../docs/traffic.mp4")
+    if (cap.isOpened()== False):
+        print("Error opening video stream or file")
+
+    while(cap.isOpened()): # this is an infinite loop
+
+        ret, frame = cap.read() # get a frame, ret will be true or false if getting succeeds
+        
+        if ret == True: # if we get a frame successfully 
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            cv2.imshow('Frame',frame) # show the image
+
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+        else:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
